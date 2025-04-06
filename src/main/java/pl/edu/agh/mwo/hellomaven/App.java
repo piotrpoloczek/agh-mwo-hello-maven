dcbbdeefa4e2071525e6ad11166889f02b0f2a4c
PiotrPoloczek <piotr@PiotrPoloczeks-MacBook-Pro.local>
2025-04-06 10:05:20 +0200

    change the target file name

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,12 +1,32 @@
 package pl.edu.agh.mwo.hellomaven;
 
+
+import com.indvd00m.ascii.render.Render;
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.elements.PseudoText;
+
+
 /**
  * Simple main application
  */
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
+        IRender render = new Render();
+        render.setPseudoCanvas(true);
+        IContextBuilder builder = render.newBuilder();
+        builder.width(120).height(20);
+        builder.element(new PseudoText("Hello World", false));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+        System.out.println(s);
+
+
+//        System.out.println("Hello World!");
     }
     
     public int getRandomNumber() {
