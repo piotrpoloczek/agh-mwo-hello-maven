f3b7e8c1a51259983d588a1cf83eca4c40f8d64a
PiotrPoloczek <piotr@PiotrPoloczeks-MacBook-Pro.local>
2025-04-06 10:50:46 +0200

    add code checkstyle to the pom

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -8,30 +8,30 @@ import com.indvd00m.ascii.render.api.IRender;
 import com.indvd00m.ascii.render.elements.PseudoText;
 
 
-/**
- * Simple main application
- */
+/// Simple main application
 public class App {
 
-    public static void main(String[] args) {
 
+    static final int WIDTH = 120;
+    static final int HEIGHT = 20;
+    static final int RANDOM_NUMBER = 4;
+
+
+    public static void main(String[] args) {
 
         IRender render = new Render();
         render.setPseudoCanvas(true);
         IContextBuilder builder = render.newBuilder();
-        builder.width(120).height(20);
+        builder.width(App.WIDTH).height(App.HEIGHT);
         builder.element(new PseudoText("Hello World", false));
         ICanvas canvas = render.render(builder.build());
         String s = canvas.getText();
         System.out.println(s);
-
-
-//        System.out.println("Hello World!");
     }
     
     public int getRandomNumber() {
         // chosen by fair dice roll.
         // guaranteed to be random
-        return 4;
+        return App.RANDOM_NUMBER;
     }
 }
